dd275b5e43f3ed89a02fdc32de3aa853d605bd64
daryaagafonova <ADashyta2013@yandex.ru>
2023-07-16 13:51:59 +0300

    calling the subtraction function in main.py

--- py_project/main.py
+++ py_project/main.py
@@ -1,8 +1,14 @@
-from funcs import addition
+from funcs import addition, subtraction
 
 num1 = int(input())
 num2 = int(input())
 
 add = addition(num1, num2)
 print(add)
+
+num3 = int(input())
+num4 = int(input())
+
+subtract = subtraction(num1, num2)
+print(subtract)
 
